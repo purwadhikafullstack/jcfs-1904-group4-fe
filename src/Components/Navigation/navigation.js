176d48deef11cf7c8a3aad6aca56f09bd03b18bb
jaysonadam <jaysonadam@gmail.com>
2022-03-31 11:26:21 +0700

    New navbar

--- src/Components/Navigation/navigation.js
+++ src/Components/Navigation/navigation.js
@@ -2,70 +2,33 @@ import React from "react";
 import {
     Nav,
     Navbar,
-    NavLink,
-    NavItem,
-    NavbarBrand,
-    Collapse,
-    NavbarToggler,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    UncontrolledDropdown,
-} from "reactstrap";
+    NavDropdown
+} from "react-bootstrap";
 
 function Navigation() {
     return (
-      <Navbar
-        color="dark"
-        expand="md"
-        dark
-      >
-      <NavbarBrand href="/">
-        reactstrap
-      </NavbarBrand>
-      <NavbarToggler onClick={function noRefCheck(){}} />
-      <Collapse navbar>
-        <Nav
-          className="me-auto"
-          navbar
-        >
-          <NavItem>
-            <NavLink href="/components/">
-              Components
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              GitHub
-            </NavLink>
-          </NavItem>
-          <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Options
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Option 1
-            </DropdownItem>
-            <DropdownItem>
-              Option 2
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>
-              Reset
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-        </Nav>
-      </Collapse>
-    </Navbar>
-    )
+      <div>
+        <Navbar bg="light" expand="lg" style={{boxShadow: '0 6px 6px 0 rgb(0, 0, 0, 0.2)'}}>
+          <Navbar.Brand href="#home" style={{paddingTop: '2.5px', marginLeft: '25px'}}>ezfurniture</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" style={{marginRight: '25px'}}/>
+          <Navbar.Collapse id="basic-navbar-nav" style={{marginLeft: '25px'}}>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <NavDropdown title="Products" id="basic-nav-dropdown" style={{marginTop: '0px', marginInline: '0px'}}>
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link">About Us</Nav.Link>
+              <Nav.Link href="#home">Login</Nav.Link>
+              <Nav.Link href="#link">Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
 };
 
 export default Navigation;
