833a8d49b69c2778fb4e240a395bb67a4c6e59ad
jaysonadam <jaysonadam@gmail.com>
2022-04-04 22:53:35 +0700

    Update client profile page

--- src/Pages/Client/client.js
+++ src/Pages/Client/client.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card } from "react-bootstrap";
+import { Card, FormControl, InputGroup } from "react-bootstrap";
 import { Button } from "@mui/material";
 
 
@@ -8,43 +8,123 @@ function Client() {
 
     return (
         <div style={{display: 'flex', justifyContent: 'center', marginTop: '30px'}}>
-            <Card style={{width: '1000px', height: '750px'}}>
+            <Card style={{width: '1000px', height: '770px'}}>
                 <Card.Header style={{fontSize: '30px', display: 'flex', justifyContent: 'center'}}>
                     <i class="bi bi-person-circle" style={{marginRight: '18px'}}></i>
                     My Profile
                 </Card.Header>
 
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-                    <div style={{display: 'flex', justifyContent: 'space-around', width: '800px'}}>
+                <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '50px'}}>
+                    <div style={{display: 'flex', flexDirection: 'column', marginInline: '30px'}}>
 
-                        <Card border="success" style={{ width: '350px', height: '250px'}}>
+                        <Card border="success" style={{ width: '450px', height: '250px'}}>
                             <Card.Header>Profile Picture</Card.Header>
                             <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
                                 <Card.Img variant="top" src="http://cdn.onlinewebfonts.com/svg/img_24787.png" style={{objectFit: 'cover', width: '100px', heigh: '100px'}}></Card.Img>
                             </div>
                             <Card.Body style={{display: 'flex', justifyContent: 'center'}}>
                                 <div>
-                                    <Button color="success" style={{marginInline: '10px'}}>Edit</Button>
-                                    <Button variant="contained" color="success" style={{marginInline: '10px'}}>Save</Button>
+                                    <Button color="success" style={{marginInline: '10px'}}>Add / Edit</Button>
                                 </div>
                             </Card.Body>
                         </Card>
-                        <Card border="success" style={{ width: '350px', height: '250px' }}>
-                            <Card.Header>Profile Data</Card.Header>
+
+                        <Card border="success" style={{width: '450px', height: '250px', marginTop: '30px'}}>
+                            <Card.Header>Address</Card.Header>
                             <Card.Body>
-                            <Card.Title>Success Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk
-                                of the card's content.
-                            </Card.Text>
+                                <div style={{display: 'flex', flexDirection: 'row'}}>
+                                    <InputGroup className="mb-3">
+                                        <FormControl
+                                        placeholder="Add New Address"
+                                        aria-label="Username"
+                                        aria-describedby="basic-addon1"
+                                        />
+                                    </InputGroup>
+                                    <Button color="success" style={{fontSize: '15px', height: '40px', marginLeft: '10px'}}>Add</Button>
+                                </div>
+                                <div style={{display: 'flex', flexDirection: 'row'}}>
+                                    <InputGroup className="mb-3">
+                                        <FormControl
+                                        placeholder="Edit Address"
+                                        aria-label="Username"
+                                        aria-describedby="basic-addon1"
+                                        />
+                                    </InputGroup>
+                                    <Button color="success" style={{fontSize: '15px', height: '40px', marginLeft: '10px'}}>Save</Button>
+                                </div>
+                                <div style={{display: 'flex', flexDirection: 'row'}}>
+                                    <select className="form-control">
+                                        <option>Address 1</option>
+                                        <option>Address 2</option>
+                                        <option>Address 3</option>
+                                    </select>
+                                    <Button color="success" style={{fontSize: '15px', height: '40px', marginLeft: '10px'}}>Delete</Button>
+                                </div>
                             </Card.Body>
                         </Card>
-
+                    </div>
+                    <div style={{marginInline: '30px'}}>
+                        <Card border="success" style={{ width: '350px', height: '530px' }}>
+                            <Card.Header>Profile Data</Card.Header>
+                            <Card.Body>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                    placeholder="Firstname"
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    value="Firstname"
+                                    />
+                                </InputGroup>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                    placeholder="Lastname"
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    value="Lastname"
+                                    />
+                                </InputGroup>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                    placeholder="Lastname"
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    value="Email"
+                                    />
+                                </InputGroup>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                    placeholder="Lastname"
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    value="Age"
+                                    />
+                                </InputGroup>
+                                <select className="form-control">
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                </select>
+                                <div style={{marginTop: '20px'}}>
+                                    <Card.Title>Default Address</Card.Title>
+                                    <select className="form-control" style={{marginTop: '13px'}}>
+                                        <option>Address 1</option>
+                                        <option>Address 2</option>
+                                        <option>Address 3</option>
+                                    </select>
+                                </div>
+                                <div style={{marginTop: '20px'}}>
+                                    <Card.Title>Edit Address</Card.Title>
+                                    <select className="form-control" style={{marginTop: '13px'}}>
+                                        <option>Address 1</option>
+                                        <option>Address 2</option>
+                                        <option>Address 3</option>
+                                    </select>
+                                </div>
+                            </Card.Body>
+                        </Card>
                     </div>
                 </div>
-
-                <div style={{marginTop: '40px', display: 'flex', justifyContent: 'center'}}>
-                    <Card border="success" style={{width: '750px', height: '250px'}}></Card>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Button color="error" style={{marginTop: '45px', width: '850px'}}>Save Changes</Button>
                 </div>
             </Card>
         </div>
