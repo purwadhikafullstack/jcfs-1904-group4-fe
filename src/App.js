0497a13850517b17e16a50128b6854ba641932ac
jaysonadam <93925334+jaysonadam@users.noreply.github.com>
2022-04-07 20:21:54 +0700

    B4-14 - B-17 (#4)
    * Create base folders
    
    * Update Folders
    
    * Update config folder
    
    * Prepare to change branch
    
    * Create Navbar
    
    * Navbar update
    
    * New navbar
    
    * Update filter & sort cards
    
    * Sort by sudah jalan
    
    * Filter, Sort by, Pagination semua diubah, sudah jalan
    
    * Product detail design selesai
    
    * Revise product management and client edit profile page
    
    * Update client profile page
    
    * Add margin profile picture
    
    * Filter, sort, pagination for products list done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -7,35 +8,48 @@ import { useDispatch } from 'react-redux';
 import Login from './Pages/Login/Login';
 import { keepLoginAction } from '../src/Store/Actions/action.js';
 
+import Admin from './Pages/Admin/admin'
+import Client from './Pages/Client/client'
+import Address from './Pages/Client/address'
+import Products from "./Pages/Products/products";
+import Navigation from "./Components/Navigation/navigation"
+import ProductDetail from "./Pages/ProductDetail/productDetail";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./style.css"
+
 function App() {
+
   const [isLocalStorageChecked, setIsLocalStorageChecked] = useState(false);
   const dispatch = useDispatch();
-
+  
   useEffect(() => {
     const userLocalStorage = localStorage.getItem('userData');
-
+  
     if (userLocalStorage) {
       const userData = JSON.parse(userLocalStorage);
       const { user, postToken } = userData;
-
+  
       // const { user_id, username, role, warehouse_id } = user;
       const action = keepLoginAction({ user, postToken });
-
+  
       dispatch(action);
     }
-
+  
     setIsLocalStorageChecked(true);
   }, []);
 
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+    return (
+        <Router>
+            <Navigation />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Products />} />
+                <Route path="/address" element={<Address />} />
+                <Route path="client" element={<Client />} />
+                <Route path="products/:product_id" element={<ProductDetail />} />
+            </Routes>
+        </Router>
+    )
+};
 
 export default App;
---
+++ src/Components/ProductCard/productCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import "./style.css"
+import {Card, Button} from "react-bootstrap";
+
+function ProductCard(props) {
+    const { product_id, product_name, price, product_image_name } = props.product
+
+    return (
+        <div>
+            <Card className="kartu" style={{ width: '250px', height: '450px', borderRadius: '5px', boxShadow: '0 4px 4px 0 rgb(0, 0, 0, 0.2)', backgroundColor: '#eaeaea', marginInline: '20px', marginBottom: '40px'}}>
+                <Card.Img variant="top" src={product_image_name} style={{width: '248px', height: '248px', objectFit: 'cover'}}/>
+                <Card.Body>
+                    <Card.Title>{product_name}</Card.Title>
+                    <Card.Subtitle style={{fontSize: '20px'}}>Rp. {price}</Card.Subtitle>
+                    <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                    <Button variant="outline-danger" style={{width: '100%'}} href={`/products/${product_id}`}>
+                        Details
+                    </Button>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '20px'}}>
+                        <Button style={{backgroundColor: 'rgb(33, 37, 41)', borderColor: 'rgb(33, 37, 41)'}}>Buy Now</Button>
+                        <Button variant="success">Add to Cart</Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+};
+
+export default ProductCard;
