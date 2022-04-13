d3f10c9c7bb3c1d2d3da4f8c47cacb66eded1437
jaysonadam <jaysonadam@gmail.com>
2022-04-13 23:17:49 +0700

    Cart page design

--- src/Pages/Cart/cart.js
+++ src/Pages/Cart/cart.js
@@ -4,7 +4,6 @@ import axios from "../../Config/axios";
 
 import { Card } from "react-bootstrap";
 import { Button } from "@mui/material";
-
 import CartBox from './cartBox'
 
 function Cart() {
@@ -27,25 +26,45 @@ function Cart() {
         getCart();
     }, [])
 
-    return (
-        <div className="d-flex justify-content-center mt-5">
-            <Card style={{width: '1000px', minHeight: '800px'}}>
-                <Card.Header style={{fontSize: '30px'}}>
-                    My Cart
-                </Card.Header>
-                <Card.Body>
-                    <div className="d-flex flex-wrap col-9">
-                        {carts.map((cart) => {
-                            <CartBox
-                                key={cart.product_id}
-                                product={cart}
-                            />
-                        })}
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
-    )
+    if (carts.length) {
+        return (
+            <>
+                <div className="d-flex justify-content-center mt-5">
+                    <Card style={{ width: '1000px', minHeight: '800px', borderRadius: '5px', marginBottom: '50px' }}>
+                        <Card.Header style={{ fontSize: '30px', paddingLeft: '20px' }}>
+                            <i class="bi bi-cart2" style={{ marginRight: '15px'}}></i>
+                            My Cart
+                        </Card.Header>
+                        <Card.Body className="p-4">
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
+                                {carts.map((cart) => (
+                                    <CartBox
+                                        key={cart.product_id}
+                                        product={cart}
+                                    />
+                                ))}
+                            </div>
+                        </Card.Body>
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button style={{ width: '300px', marginBottom: '20px'}}
+                                    variant="contained"
+                                    color="success"
+                            >
+                                Checkout Now!
+                            </Button>
+                        </div>           
+                    </Card>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Your cart is still empty</h1>
+                <i class="bi bi-emoji-frown"></i>
+            </div>
+        )
+    }
 
 };
 
--- src/Pages/Cart/cartBox.js
+++ src/Pages/Cart/cartBox.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import './cartBox.css';
 import { Card } from "react-bootstrap";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import { Button, IconButton } from "@mui/material";
 
 function CartBox(props) {
 
     const { product_id, product_name, product_price, product_image_name, quantity } = props.product
-    console.log(product_id)
-    console.log(quantity)
+    const [totalPrice, setTotalPrice] = useState([]);
+
+    const addTotalPrice = () => {
+        setTotalPrice(product_price * quantity)
+    };
+
+    useEffect(() => {
+        addTotalPrice();
+    }, [])
 
     return (
-        <Card style={{ boxShadow: '0 6px 6px 0 rgb(0, 0, 0, 0.2)', height: '200px', backgroundColor: 'black'}}>
-            <Card.Title>{product_name}</Card.Title>
-            <Card.Subtitle>{product_price}</Card.Subtitle>
-        </Card>
+        <div>
+            <Card className="cart-box">
+                <Card.Img src={product_image_name} className="image"></Card.Img>
+                <div className="card-body">
+                    <Card.Title>{product_name}</Card.Title>
+                    <Card.Subtitle className="mt-1">Product ID: {product_id}</Card.Subtitle>
+                    <Card.Title className="mt-4">Price: {product_price}</Card.Title>
+                </div>
+                <div className="quantity">
+                    <Card.Title>Quantity: {quantity}</Card.Title>
+                    <div className="button">
+                        <IconButton>
+                            <AddIcon color="success"/>
+                        </IconButton>
+                        <IconButton disabled={quantity === 1}>
+                            <RemoveIcon color="success"/>
+                        </IconButton>
+                    </div>
+                    <Card.Title>Total Price: {totalPrice}</Card.Title>
+                </div>
+            </Card>
+        </div>
     )
 };
 
