fc54bd83d299b9c8cf091cb8fb03e4e1a1fb1169
jaysonadam <jaysonadam@gmail.com>
2022-04-18 17:57:33 +0700

    Checkout page aman

--- src/Pages/Checkout/checkout.js
+++ src/Pages/Checkout/checkout.js
@@ -4,9 +4,11 @@ import axios from "../../Config/axios";
 
 import './style.css'
 import { Card } from "react-bootstrap";
+import { Button } from "@mui/material";
 
 function Checkout() {
     const user_id = useSelector((state) => state.auth.user_id);
+    const [client, setClient] = useState([]);
 
     const [defaultAddress, setDefaultAddress] = useState([]);
     const [userCart, setUserCart] = useState([]);
@@ -17,16 +19,19 @@ function Checkout() {
         totalPrice: 0
     });
     const [paymentMethod, setPaymentMethod] = useState({
-        method: ''
+        method: 'bank_transfer'
     });
     const [chooseAddress, setChooseAddress] = useState({
         address_id: ''
     });
 
+    console.log(paymentMethod)
+
     useEffect(() => {
         getDefaultAddress();
         getCart();
         getAddress();
+        getClient();
     }, []);
 
     useEffect(() => {
@@ -34,6 +39,17 @@ function Checkout() {
             getChosenAddress()
         }
     }, [chooseAddress])
+
+    const getClient = async () => {
+        try {
+            const res = await axios.get(`/users/get/${user_id}`)
+            const { data } = res;
+
+            setClient(data.user[0]);
+        } catch (error) {
+            console.log(alert(error.message))
+        }
+    };
 
     const getDefaultAddress = async () => {
         try {
@@ -136,7 +152,7 @@ function Checkout() {
                             )}
                         </select>
 
-                        <Card.Title className="mt-4 mb-3">
+                        <Card.Title className="mt-5 mb-3">
                             <i class="bi bi-cart4" style={{ marginRight: '10px' }}></i>
                             Products :
                         </Card.Title>
@@ -156,22 +172,37 @@ function Checkout() {
                                 </tr>
                             )}
                         </table>
-
-                        <Card.Title className="mt-3">Total + 5% Tax : Rp. {totalState.totalPrice}</Card.Title>
+                        
+                        <Card.Subtitle className="mt-4">Total Price : Rp. {totalState.subTotal}</Card.Subtitle>
+                        <Card.Subtitle className="mt-2">Tax (5%) : Rp. {totalState.tax} </Card.Subtitle>
+                        <Card.Title className="mt-2">Subtotal : Rp. {totalState.totalPrice}</Card.Title>
                     </Card.Body>
                 </Card>
 
-                <Card style={{ width: '750px', height: '750px', marginLeft: '30px' }}>
+                <Card style={{ width: '750px', height: '425px', marginLeft: '30px' }}>
                     <Card.Header style={{ fontSize: '25px' }}>
                         <i class="bi bi-credit-card" style={{ marginRight: '13px' }}></i>
                         Checkout
                     </Card.Header>
                     <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', padding: '20px' }}>
+                        
                         <Card.Title>
-                            Please choose a payment method
+                            <i class="bi bi-person-circle" style={{ marginRight: '10px' }}></i>
+                            Billing Account / Recipient :
+                        </Card.Title>
+                        {client.gender === "male" && (
+                            <Card.Subtitle className="mt-2">Mr. {client.full_name}</Card.Subtitle>
+                        )}
+                        {client.gender === "female" && (
+                            <Card.Subtitle className="mt-2">Ms. {client.full_name}</Card.Subtitle>
+                        )}
+                        <Card.Subtitle className="mt-2 mb-5">Email : {client.email}</Card.Subtitle>
+                            
+                        <Card.Title>
+                            Please choose a payment method :
                         </Card.Title>
 
-                        <select className="form-control mt-3" name="method" onSelect={handleMethod}>
+                        <select className="form-control mt-3" name="method" onChange={handleMethod}>
                             <option value="bank_transfer">Bank Transfer</option>
                             <option value="credit_card">Credit Card</option>
                             <option value="debit_card">Debit Card</option>
@@ -183,6 +214,17 @@ function Checkout() {
                             Total Payment : Rp. {totalState.totalPrice}
                         </Card.Title>
                     </Card.Body>
+
+                    <div className="d-flex justify-content-center">
+                        <Button style={{ width: '300px', marginBottom: '20px' }}
+                                variant="contained"
+                                color="error"
+                                // onClick={}
+                        >
+                            Confirm Order
+                        </Button>
+                    </div>
+
                 </Card>
 
             </div>
